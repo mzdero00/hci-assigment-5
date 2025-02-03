5c7e0a2fdf8879f4c170555f2d70f2a30cf1bac8
mzdero00 <92402511+mzdero00@users.noreply.github.com>
2025-02-03 04:50:28 +0100

    check2

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,7 +1,7 @@
-export default function ShowcasePage() {
+export default function SupportPage() {
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
-      <h1 className="text-6xl font-extrabold tracking-tight">Showcase</h1>
+      <h1 className="text-6xl font-extrabold tracking-tight">Support</h1>
     </main>
   );
 }
